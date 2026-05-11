14212158bc0a9eec3e4157bad1520929755feafa
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-12 00:13:27 +0500

    refactor(query): update query module

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,9 +1,19 @@
 mod builder;
 mod filter;
+mod hints;
+mod index_recommender;
+mod optimizer;
 mod projection;
+mod stats;
 mod types;
+mod zero_copy;
 
 pub use builder::QueryBuilder;
 pub use filter::{parse_field_filter, Filter};
+pub use hints::{OptimizationHint, QueryAnalyzer, QueryHint};
+pub use index_recommender::{IndexRecommendation, IndexRecommender, QueryStats};
+pub use optimizer::{OptimizedQuery, QueryOptimizer};
 pub use projection::Projection;
+pub use stats::{CollectionStats, FilterPattern, QueryStatistics};
 pub use types::{Cursor, OrderBy, PaginatedResult, SortDirection};
+pub use zero_copy::{DirectDeserializer, LazyValue, TypedArrayDeserializer, ZeroCopyDeserialize};
