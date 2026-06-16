906afddd8ec44a29337c061aa3b31ee4e1ba046b
Dmitriy <dmitriy@example.com>
2026-06-16 16:35:50 +0500

    fmt: format nosql_orm_derive

--- nosql_orm_derive/src/lib.rs
+++ nosql_orm_derive/src/lib.rs
@@ -7,6 +7,36 @@ mod validate;
 use entity::{generate_entity, generate_model, generate_orm_entity_macro};
 use validate::generate_validate;
 
+fn derive_model_helper(item: &syn::DeriveInput) -> Result<proc_macro::TokenStream, Vec<syn::Error>> {
+    let name = &item.ident;
+    let (impl_generics, ty_generics, where_clause) = &item.generics.split_for_impl();
+    
+    Ok(quote::quote! {
+        impl #impl_generics model_helper::ModelHelper for #name #ty_generics #where_clause {
+            fn validate(&self) -> Vec<model_helper::ValidationError> {
+                Vec::new()
+            }
+            
+            fn before_insert(&mut self) {}
+            fn before_update(&mut self) {}
+            fn after_load(&mut self) {}
+            fn transform(&mut self) {}
+        }
+    }.into())
+}
+
+#[proc_macro_derive(ModelHelper)]
+pub fn derive_model_helper_macro(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as syn::DeriveInput);
+    match derive_model_helper(&input) {
+        Ok(tokens) => tokens,
+        Err(errors) => {
+            let compile_errors = errors.iter().map(|e| e.to_compile_error());
+            proc_macro::TokenStream::from(quote::quote! { #(#compile_errors)* })
+        }
+    }
+}
+
 #[proc_macro_derive(
   Entity,
   attributes(
